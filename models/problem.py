6c8a86b3c31f6e73a20a7b65bb51d7d6d25f4441
Eddie <951001117@qq.com>
2022-04-03 00:34:22 +0800

    fix typos

--- models/problem.py
+++ models/problem.py
@@ -6,7 +6,7 @@ class Problem(models.Model):
     id = models.AutoField(primary_key=True)
     description = models.CharField(max_length=200, blank=True)
     type = models.CharField(max_length=20, default="单选",
-                            choices=[("单选", "单选"), ("多选", "多选"), ("填空", "填空"), ("判断", "判断")])
+                            choices=[("single", "单选"), ("multiple", "多选"), ("completion", "填空"), ("binary", "判断")])
     A = models.CharField(max_length=50, null=True)
     B = models.CharField(max_length=50, null=True)
     C = models.CharField(max_length=50, null=True)
