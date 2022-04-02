3ce8f6398a6139516a7a37f5f7563c18bbd090c1
Eddie <951001117@qq.com>
2022-04-03 00:35:11 +0800

    processed a function

--- utils/auxilary.py
+++ utils/auxilary.py
@@ -2,6 +2,12 @@ from django.http import JsonResponse
 
 
 def msg_response(ret, msg=None):
+    if ret == 2:
+        return JsonResponse({'ret': ret,
+                             'msg': '登录过期'})
+    if ret == 3:
+        return JsonResponse({'ret': ret,
+                             'msg': '参数错误'})
     if msg is None:
         return JsonResponse({'ret': ret})
     return JsonResponse({'ret': ret, 'msg': msg})
