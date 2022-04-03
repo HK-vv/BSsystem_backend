440e01512515e726088b29466607fd296bd7558f
HKvv <823997462@qq.com>
2022-04-03 21:57:27 +0800

    fixed bug

--- utils/decorators.py
+++ utils/decorators.py
@@ -24,7 +24,7 @@ def require_admin_login():
     def decorator(func):
         @wraps(func)
         def inner(request, *args, **kwargs):
-            if not request.user.is_authenticated():
+            if not request.user.is_authenticated:
                 return msg_response(2)
             try:
                 return func(request, *args, **kwargs)
@@ -42,9 +42,9 @@ def require_super_login():
     def decorator(func):
         @wraps(func)
         def inner(request, *args, **kwargs):
-            if not request.user.is_authenticated():
+            if not request.user.is_authenticated:
                 return msg_response(2)
-            if not request.user.is_superuser():
+            if not request.user.is_superuser:
                 return msg_response(1, "权限不足")
             try:
                 return func(request, *args, **kwargs)
