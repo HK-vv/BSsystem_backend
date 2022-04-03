9c7c1e76c24b1619c05f00148a3980a5c61fe429
Eddie <951001117@qq.com>
2022-04-03 19:01:10 +0800

    complete manual

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 这是BrainStorm系统的后端开发仓库.
 
 主仓库: https://github.com/HK-vv/SE-BSsystem
+
+
+
+### 使用说明书
+
+在使用前，请向后端管理员申请获得`secret.json`文件，并将其放置在`BSsystem_backend/`目录下。没有此文件无法正常运行。请不要传播`secret.json`文件。
+
+#### 指令运行
+
+```cmd
+git clone git@github.com:HK-vv/BSsystem_backend.git
+cd BSsystem_backend 
+
+pip install django
+pip install requests
+
+python manage.py makemigrations models
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8080
+```
+
+输入以上指令后，即已运行后端，可以测试接口。
+
+web端测试跳转，请将`dist`文件夹放置在`BSsystem_backend/`目录下，并保证主页路径为`BSsystem_backend/dist/index.html`后，打开[网址](http://localhost:8080/index.html)`http://localhost:8080/index.html`。
+
+若无法运行，请联系管理员说明问题。
--- brainstorm/urls.py
+++ brainstorm/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/user/', include('user_app.urls')),
     path('api/admin/', include('admin_app.urls')),
     path('api/general/', include('general_app.urls')),
-]
+] + static("/", document_root='./dist')
