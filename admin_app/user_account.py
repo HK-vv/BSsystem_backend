88a456fccfe3c2d20e8057d1bd0c82c58a4cbf93
HKvv <823997462@qq.com>
2022-04-06 16:29:03 +0800

    func A finish without one api unfilled

--- admin_app/user_account.py
+++ admin_app/user_account.py
@@ -1,9 +1,32 @@
+from django.core.paginator import Paginator
+from django.db.models import Count
+
+from bsmodels.models import BSUser
+from utils.auxilary import ret_response
 from utils.decorators import require_admin_login
 
 
 @require_admin_login()
 def user_list(request, data):
-    pass
+    ps = int(data['pagesize'])
+    pn = int(data['pagenum'])
+    kw = data.get('keyword')
+    st = data.get('sort_by_rating')
+
+    lst = BSUser.objects.all()
+    if kw:
+        lst = lst.filter(username__icontains=kw)
+    if st == 'descending':
+        lst = lst.order_by('-rating')
+    elif st == 'ascending':
+        lst = lst.order_by('rating')
+
+    tot = lst.count()
+    paginator = Paginator(lst, ps)
+    page = paginator.page(pn)
+    items = page.object_list.values('username', 'rating', matches=Count('registration'))
+
+    return ret_response(0, {'items': items, 'total': tot})
 
 
 @require_admin_login()
