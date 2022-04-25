ffdd1110eb4d0b09677ceaa41c74c2d05eba53e7
HKvv <823997462@qq.com>
2022-04-25 20:27:57 +0800

    upd ip output

--- utils/auxilary.py
+++ utils/auxilary.py
@@ -1,8 +1,10 @@
 import datetime
 import json
+import re
 
 import pytz
 from django.http import JsonResponse
+import requests
 
 from brainstorm.settings import OUTPUT_LOG
 
@@ -64,6 +66,22 @@ def get_ip(request):
     return ip
 
 
+def output_ip_info(request):
+    ip = get_ip(request)
+    print(f"Request received from {ip}")
+    url = "https://www.ip138.com/iplookup.asp?ip={}&action=2".format(ip)
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36'
+    }
+
+    response = requests.get(url=url, headers=headers)
+    response.encoding = 'gb2312'
+
+    for match in re.finditer('"(ASN归属地|yunyin|idc|prov|city|ct)":"(.*?)"', response.text):
+        print(match.group(), end=', ')
+    print()
+
+
 def output_request_info(request):
-    if OUTPUT_LOG:
-        print(f"Request received from {get_ip(request)}")
+    output_ip_info(request)
