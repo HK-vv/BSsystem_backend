46c18c47d4438a3cc1d181562100c3da13906660
HKvv <823997462@qq.com>
2022-05-08 21:26:45 +0800

    add rating system

--- bsmodels/models.py
+++ bsmodels/models.py
@@ -8,10 +8,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models, transaction
 from django.db.models import Max, Avg, Min
 
-from brainstorm.settings import OUTPUT_LOG, UPDATE_INTERVAL
+from brainstorm.settings import OUTPUT_LOG, UPDATE_INTERVAL, INITIAL_RATING
 from utils.auxiliary import get_current_time
 from utils.exceptions import SubmitWrongProblemError, ContestFinishedError
-from utils.rating import calc_rating_result
+from utils.rating import RatingSystem
 
 
 class BSUser(models.Model):
@@ -297,11 +297,21 @@ class Contest(models.Model):
         blst = {}
         rlst = {}
         for reg in regs:
-            if reg.starttime is not None:
+            # only consider users made submit
+            if reg.timecost is not None:
                 blst[reg.userid_id] = reg.userid.rating
                 rlst[reg.userid_id] = reg.rank
 
-        alst = calc_rating_result(rank=rlst, blst=blst)
+        n = len(rlst)
+        remain = GlobalVars.get('STIMULATION')
+        # give more stimulation on open contest
+        stim_rate = 4 if self.password is None else 16
+        stim = min(remain, int(n * INITIAL_RATING / stim_rate))
+        remain -= stim
+        rsystem = RatingSystem(ranks=rlst, before_ratings=blst, stimulation=stim)
+        alst, stim = rsystem.get_new_ratings()
+        remain += stim
+        GlobalVars.put('STIMULATION', remain)
 
         for reg in regs:
             reg.beforerating = blst[reg.userid_id]
@@ -582,3 +592,27 @@ def update_rank():
         if OUTPUT_LOG:
             print(f'rating排行榜更新失败')
         raise e
+
+
+class GlobalVars(models.Model):
+    key = models.CharField(max_length=100)
+    value = models.IntegerField(null=True)
+
+    @classmethod
+    def get(cls, key):
+        var = cls.objects.get(key=key)
+        return var.value
+
+    @classmethod
+    def put(cls, key, value):
+        try:
+            var = cls.objects.get(key=key)
+        except cls.DoesNotExist:
+            var = cls.objects.create(key=key)
+        var.value = value
+        var.save()
+
+    @classmethod
+    def update(cls, key, delta):
+        value = cls.get(key)
+        cls.put(key, value + delta)
--- utils/rating.py
+++ utils/rating.py
@@ -1,18 +1,106 @@
-def calc_rating_result(rank, blst):
-    tot = len(rank)
-    r = {}
-    for p, ra in blst.items():
-        diff = 0
-        rk = rank[p]
-        if rk <= tot * 5 // 75:
-            diff = 5
-        elif rk <= tot * 15 // 75:
-            diff = 4
-        elif rk <= tot * 30 // 75:
-            diff = 3
-        elif rk <= tot * 50 // 75:
-            diff = 2
-        elif rk <= tot * 75 // 75:
-            diff = 1
-        r[p] = ra + diff
-    return r
+from math import sqrt
+
+
+class RatingSystem:
+    """
+    Attributes of this class is not stable, do not use them.
+    """
+    participants = []
+    rank = {}
+    ratings = {}
+    d = {}
+    stimulation = 0
+
+    def __init__(self, ranks: dict, before_ratings: dict, stimulation: int = None):
+        ordered = sorted(ranks.items(), key=lambda it: it[1])
+        self.participants = []
+        for x in ordered:
+            self.participants.append(x[0])
+        self.rank = ranks.copy()
+        self.ratings = before_ratings.copy()
+        self.participants.append(-1)
+        self.rank[-1] = max(ranks.values()) + 1
+        self.ratings[-1] = stimulation if stimulation is not None else 0
+        self.__rearrange_ranks()
+
+    def get_new_ratings(self):
+        self.__calc_diff()
+        r = {}
+        for i in self.participants:
+            r[i] = self.ratings[i] + self.d[i]
+        r.pop(-1)
+        rem = self.d[-1]
+        return r, rem
+
+    def __calc_diff(self):
+        orded_by_rating = sorted(self.ratings.items(), key=lambda it: it[1], reverse=True)
+        n = len(self.participants)
+
+        for i in range(len(orded_by_rating)):
+            orded_by_rating[i] = orded_by_rating[i][0]
+        for i in self.participants:
+            sd = self.__calc_seed(i)
+            rk = self.rank[i]
+            m = round(sqrt(sd * rk))
+            R = self.ratings[orded_by_rating[m - 1]]
+            act = sd - rk
+            self.d[i] = (R - self.ratings[i]) // 2 + int(act * 10)
+
+        # Total sum should not be more than zero.
+        sum = 0
+        for i in self.participants:
+            sum += self.d[i]
+        inc = -sum // n
+        for i in self.participants:
+            self.d[i] += inc
+
+        # Sum of top-4*sqrt should be adjusted to zero.
+        sum = 0
+        zero_sum_count = min(int(4 * round(sqrt(n))), n)
+        for i in range(0, zero_sum_count):
+            sum += self.d[orded_by_rating[i]]
+        inc = min(max(-sum // zero_sum_count, -10), 0)
+        for i in self.participants:
+            self.d[i] += inc
+
+    def __calc_P(self, x, y):
+        return 1 / (1 + 10 ** ((self.ratings[y] - self.ratings[x]) / 400))
+
+    def __calc_seed(self, pid):
+        sd = 1
+        for i in self.participants:
+            if i != pid:
+                sd += self.__calc_P(i, pid)
+        return sd
+
+    def __rearrange_ranks(self):
+        new_rk = {}
+        successive_ordered_rank = sorted(self.rank.items(), key=lambda x: x[1])
+        p = 1
+        for k, v in successive_ordered_rank:
+            new_rk[k] = p
+            p += 1
+        self.rank = new_rk
+
+
+if __name__ == '__main__':
+    rlst = {}
+    blst = {}
+    for i in range(1, 11):
+        rlst[i] = i
+        blst[i] = 0
+    while True:
+        input("press enter")
+        print("contest result:")
+        print('rlst:', rlst)
+        print('blst:', blst)
+
+        print("after contest ratings")
+        rs = RatingSystem(ranks=rlst, before_ratings=blst, stimulation=5000)
+        nlst = rs.get_new_ratings()
+        delta = {}
+        for i in blst.keys():
+            delta[i] = nlst[i] - blst[i]
+        print('new ratings:', nlst)
+        print('delta:', delta)
+        blst = nlst
